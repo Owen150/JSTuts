80c948e49dfaae83b3d6e466d11d763cca8b2ace
Michael Owen Oduor <michaeloduor50@gmail.com>
2023-07-22 22:40:35 +0300

    Random Number guesser complete

--- website/random.js
+++ website/random.js
@@ -1,4 +1,22 @@
 const prompt = require("prompt-sync")();
 
-const target = Math.random();
-console.log(target);
+const target = Math.round(Math.random() * 100);
+
+let guesses = 0;
+
+while (true) {
+    guesses++;
+
+    const guess = Number(prompt("Guess the number (0-100): "));
+    if (guess > target) {
+        console.log("Your guess is too high.");
+        continue;
+    } else if (guess < target) {
+        console.log("Your guess is too low.");
+        continue;
+    } 
+    console.log("You got it right!");
+    break;
+}
+
+console.log("You got it right in", guesses, "tries!");
