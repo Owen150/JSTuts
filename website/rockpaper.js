7d63d9c1e90a53a46fab14083f750cfc770d654f
Michael Owen Oduor <michaeloduor50@gmail.com>
2023-07-22 23:59:24 +0300

    playerChoice and computerChoice logic

--- website/rockpaper.js
+++ website/rockpaper.js
@@ -4,7 +4,25 @@ let wins = 0;
 let losses = 0;
 let ties = 0;
 
-const choice = prompt("Enter rock, paper or scissors: ");
-if (choice !== "rock" && choice !== "paper" && choice !== "scissors") {
-    console.log("Please enter a valid choice.");
+const playerChoice = prompt("Enter rock, paper or scissors: ");
+if (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissors") {
+    console.log("Please enter a valid player choice.");
+}
+
+const choices = ["rock", "paper", "scissors"];
+//Generates a random number between 0 and 2
+const choiceIndex = Math.round(Math.random() * 2);
+const computerChoice = choices[choiceIndex];
+console.log(computerChoice);
+
+if (playerChoice === computerChoice) {
+    console.log("Draw!");
+    ties++;
+} else if (
+    (playerChoice === "paper" && computerChoice === "rock") ||
+    (playerChoice === "rock" && computerChoice === "scissors") ||
+    (playerChoice === "scissors" && computerChoice === "paper")
+) {
+    console.log("You Won!");
+    wins++;
 }
