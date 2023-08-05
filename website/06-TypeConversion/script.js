9d24dbf00a78363d00ab0f547aed4829b6cffd58
Michael Owen Oduor <michaeloduor50@gmail.com>
2023-08-05 19:13:11 +0300

    Type Conversion

---
+++ website/06-TypeConversion/script.js
@@ -0,0 +1,18 @@
+let amount = 1;
+
+// Convert String to Number
+// amount = parseInt(amount);
+// amount = +amount;
+// amount = Number(amount);
+
+// Convert Number to String
+// amount = amount.toString();
+// amount = String(amount);
+
+// Convert String to Decimal
+// amount = parseFloat(amount);
+
+// Convert Number to Boolean 1-True, 0-False
+amount = Boolean(amount);
+
+console.log(amount, typeof amount);
