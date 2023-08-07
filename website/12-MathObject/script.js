a7914253eb90ba58efba1e6cc8567046a97ecb98
Michael Owen Oduor <michaeloduor50@gmail.com>
2023-08-07 17:57:00 +0300

    Math Object

---
+++ website/12-MathObject/script.js
@@ -0,0 +1,24 @@
+let x;
+
+x = Math.sqrt(9);
+
+x = Math.abs(-5);
+
+x = Math.round(4.2);
+
+x = Math.ceil(4.2);
+
+x = Math.floor(4.2);
+
+x = Math.pow(3, 4);
+
+x = Math.min(3, 4, 5);
+
+x = Math.max(3, 4, 5);
+
+x = Math.random();
+
+// Genertes a random number between 0 and 10
+x = Math.floor(Math.random() * 10 + 1);
+
+console.log(x);
